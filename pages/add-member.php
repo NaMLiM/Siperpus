a67d0f3f31964533498cde95ab39dc3ae4bf92fe
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-12-23 00:03:53 +0700

    change input name

--- pages/add-member.php
+++ pages/add-member.php
@@ -1,145 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Siperpus | Dashboard</title>
-  <link rel="shortcut icon" type="image/jpg" href="assets/img/SiperpusLogo.png"/>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="lib/plugins/fontawesome-free/css/all.min.css">
-  <!-- IonIcons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="lib/css/adminlte.min.css">
-</head>
-<body class="hold-transition sidebar-collapse ">
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Sistem Informasi Perpustakaan</a>
-      </li>
-    </ul>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item dropdown user-menu">
-        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-          <img src="assets/img/SiperpusLogo.png" class="user-image img-circle elevation-2" alt="User Image">
-          <span class="d-none d-md-inline">nama_user</span>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <!-- User image -->
-          <li class="user-header bg-primary">
-            <img src="assets/img/SiperpusLogo.png" class="img-circle elevation-2" alt="User Image">
-            <p>
-              nama_user
-              <small>username</small>
-            </p>
-          </li>
-          <!-- Menu Body -->
-          <!-- Menu Footer-->
-          <li class="user-footer">
-            <a href="#" class="btn btn-default btn-block">Logout</a>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Pendaftaran Anggota</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Siperpus</a></li>
-              <li class="breadcrumb-item active">Pendaftaran Anggota</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container">
-          <div class="col-lg-12">
-            <div class="card">
-            <form>
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="exampleInputEmail1">NIK</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Masukkan NIK">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Nama</label>
-                    <input class="form-control" type="text" placeholder="Masukkan nama">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Nomor Telepon</label>
-                    <input class="form-control" type="text" placeholder="Masukkan nomor telepon">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Username</label>
-                    <input class="form-control" type="text" placeholder="Masukkan Username">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan Password">
-                  </div>
-                </div>
-                <!-- /.card-body -->
-
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
-      </div>
-      <!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Pendaftaran Anggota</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Siperpus</a></li>
+            <li class="breadcrumb-item active">Pendaftaran Anggota</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
-  <!-- /.content-wrapper -->
+  <!-- /.content-header -->
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2020 Akiestow Team.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Siperpus Engine Version</b> 1.0a-preview
+  <!-- Main content -->
+  <div class="content">
+    <div class="container">
+        <div class="col-lg-12">
+          <div class="card">
+            <forma ction="../system/add-member-proses.php" method="post">
+              <div class="card-body">
+                <div class="form-group">
+                  <label>NIK</label>
+                  <input type="text" class="form-control" name="NIK" placeholder="Masukkan NIK">
+                </div>
+                <div class="form-group">
+                  <label>Nama</label>
+                  <input class="form-control" type="text" name="Nama" placeholder="Masukkan nama">
+                </div>
+                <div class="form-group">
+                  <label>Nomor Telepon</label>
+                  <input class="form-control" type="text" name="NoHP" placeholder="Masukkan nomor telepon">
+                </div>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text" name="username" placeholder="Masukkan Username">
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control" name="password" placeholder="Masukkan Password">
+                </div>
+              </div>
+              <!-- /.card-body -->
+
+              <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- /.col-md-6 -->
     </div>
-  </footer>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
 </div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="lib/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="lib/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE -->
-<script src="lib/js/adminlte.js"></script>
-<!-- OPTIONAL SCRIPTS -->
-<script src="lib/plugins/chart.js/Chart.min.js"></script>
-
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="lib/js/pages/dashboard3.js"></script>
-</body>
-</html>
+<!-- /.content-wrapper -->
