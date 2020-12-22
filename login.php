b0f6f9b02570f80a3eb4deed895679f7268715d7
Nafi'ul Anam <anamnafiul99@gmail.com>
2020-12-23 00:18:52 +0700

    change icon location

--- login.php
+++ login.php
@@ -24,23 +24,22 @@
     </div>
     <div class="card-body">
       <p class="login-box-msg">Silahkan Login</p>
-
       <form action="system/login-proses.php" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Username" name="username">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
+          <input type="text" class="form-control" placeholder="Username" name="username">
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+          <input type="password" class="form-control" placeholder="Password" name="password">
         </div>
         <div>
             <button type="submit" class="btn btn-primary btn-block">Login</button>
