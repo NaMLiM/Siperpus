bd9ca4d1212a562b3d4b9b928fa920449d6a68ce
faynald <72617106+faynald@users.noreply.github.com>
2020-12-26 14:43:23 +0700

    Rename Header

--- pages/return-book.php
+++ pages/return-book.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Peminjaman Buku</h1>
+          <h1 class="m-0">Pengembalian Buku</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="index.php">Siperpus</a></li>
-            <li class="breadcrumb-item active">Peminjaman Buku</li>
+            <li class="breadcrumb-item active">Pengembalian Buku</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
