5635897ff2e9c09af033e1d576a5b4817a37778b
Nafi'ul Anam <anamnafiul99@gmail.com>
2021-01-05 12:38:55 +0700

    make NIK mandatory

--- system/add-member-proses.php
+++ system/add-member-proses.php
@@ -7,21 +7,30 @@ $noHP = $_POST["NoHP"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$insert = mysqli_query($connection, "INSERT INTO anggota VALUES ('$id_anggota', '$nama', '$noHP', '$username', '$password')");
-
-if ($insert) {
+if (!isset($_POST["ID_Anggota"])) {
 ?>
     <script>
-        alert("Pendaftaran Anggota Berhasil !");
-        document.location = "../index.php";
-    </script>
-<?php
-} else {
-?>
-    <script>
-        alert("Pendaftaran Anggota Gagal !");
+        alert("NIK Wajib Diisi !");
         document.location = "../index.php?page=add-member";
     </script>
+    <?php
+} else {
+    $insert = mysqli_query($connection, "INSERT INTO anggota VALUES ('$id_anggota', '$nama', '$noHP', '$username', '$password')");
+
+    if ($insert) {
+    ?>
+        <script>
+            alert("Pendaftaran Anggota Berhasil !");
+            document.location = "../index.php";
+        </script>
+    <?php
+    } else {
+    ?>
+        <script>
+            alert("Pendaftaran Anggota Gagal !");
+            document.location = "../index.php?page=add-member";
+        </script>
 <?php
+    }
 }
 ?>
